b14a3bcfe79983d8803d9d3671f386576b099eba
Sebastián Badea <sebastian.badea@infojobs.net>
2016-05-13 12:03:12 +0200

    Added views outlets

--- SVGKit Swift Demo/ViewController.swift
+++ SVGKit Swift Demo/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var svgkFastImageView: UIView!
+    @IBOutlet weak var svgkLayeredImageView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
